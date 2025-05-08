33beac10e9d8ee04be0a14f59d14ddfecbc3a362
Anish Araz <anisharaz919@gmail.com>
2025-05-08 18:46:25 +0530

    FEAT: add PKCE in oauth flow

--- main_app/app/(authorization-server)/oauth/v2/token/route.ts
+++ main_app/app/(authorization-server)/oauth/v2/token/route.ts
@@ -1,15 +1,14 @@
 import prisma from "@/lib/db";
 import { GenerateAccessToken } from "@/lib/oauth";
-
+import { hash } from "crypto";
 export async function GET(request: Request) {
-  // TODO code challenge verifier
   const { searchParams } = new URL(request.url);
   const clientId = searchParams.get("client_id");
   const clientSecret = searchParams.get("client_secret");
-  const grantToken = searchParams.get("grantToken");
-  console.log(clientId, clientSecret, grantToken);
+  const grantToken = searchParams.get("grant_token");
+  const code_verifier = searchParams.get("code_verifier");
 
-  if (!clientId || !clientSecret || !grantToken) {
+  if (!clientId || !clientSecret || !grantToken || !code_verifier) {
     return new Response("Missing required parameters", { status: 400 });
   }
 
@@ -27,30 +26,55 @@ export async function GET(request: Request) {
   const grantTokenFromDB = await prisma.oauthGrantToken.findUnique({
     where: {
       oauthclientId: clientId,
-      grantToken: grantToken as string,
+      grantToken: grantToken,
+    },
+    include: {
+      codeChallenge: true,
     },
   });
 
+  // TODO update the challenge method resolution to better one
+  const code_challenge_create = hash(
+    grantTokenFromDB?.codeChallenge.codeChallengeMethod == "S256"
+      ? "sha256"
+      : "sha256",
+    Buffer.from(code_verifier)
+  );
   if (!grantTokenFromDB) {
     return new Response("Invalid Grant token", { status: 400 });
   }
 
-  const accessToken = await prisma.oauthAccessToken.create({
-    data: {
-      accessToken: GenerateAccessToken(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      scope: grantTokenFromDB?.scope as string,
-      oauthclientId: clientId,
-      userId: grantTokenFromDB?.userId as string,
-    },
+  if (grantTokenFromDB.codeChallenge.codeChallenge !== code_challenge_create) {
+    return new Response("invalid code verifier", { status: 400 });
+  }
+
+  let access_token = "";
+  await prisma.$transaction(async (tx) => {
+    const accessToken = await tx.oauthAccessToken.create({
+      data: {
+        accessToken: GenerateAccessToken(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        scope: grantTokenFromDB?.scope as string,
+        oauthclientId: clientId,
+        userId: grantTokenFromDB?.userId as string,
+      },
+    });
+    access_token = accessToken.accessToken;
+    await tx.oauthGrantToken.delete({
+      where: {
+        id: grantTokenFromDB.id,
+      },
+    });
+    await tx.codeChallenge.delete({
+      where:{
+        id: grantTokenFromDB.codeChallengeId,
+      }
+    })
   });
 
-  return new Response(
-    JSON.stringify({ access_token: accessToken.accessToken }),
-    {
-      headers: { "Content-Type": "application/json" },
-      status: 200,
-    }
-  );
+  return new Response(JSON.stringify({ access_token: access_token }), {
+    headers: { "Content-Type": "application/json" },
+    status: 200,
+  });
 }
