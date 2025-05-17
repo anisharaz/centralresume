30ed7012d75712445b9a567d34a641e2736cca4a
PiyushXCoder <piyush.raj.kit@gmail.com>
2025-05-18 00:24:51 +0530

    FIX: missing route

--- native_protocol_backend/src/router/routes.ts
+++ native_protocol_backend/src/router/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import indexRouter from './index';
+import resumeRouter from './v1/resume';
 
 const routes = Router();
 
 routes.use('/', indexRouter);
+routes.use('/v1', resumeRouter);
 
 export default routes;
