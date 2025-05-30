b4ad0252a0bb27351c21a8ef46a5c51d684961dc
PiyushXCoder <piyush.raj.kit@gmail.com>
2025-05-30 22:14:12 +0530

    UI: fix responsive view for small screen : main_app

--- main_app/components/landing-page.tsx
+++ main_app/components/landing-page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import Footer from "./footer";
-
 const benefits = [
   {
     title: "Different jobs need tailored resume versions",
@@ -32,7 +31,7 @@ const benefits = [
       "We have created API which the job platforms like wellfound or any other can use to integrate a button called LOGIN WITH CENTRAL#RESUME on their platform. This button allows you to share your resume with the job portal as easily as you do with login with google. This removed the pain of needing to create a resume profile on every job platform that you use. #Create once use everywhere.",
     image: "/3.svg",
     background:
-      "bg-[radial-gradient(ellipse_at_top_right,_#3b82f6,_transparent,_#db2777)]",
+      "bg-[radial-gradient(circle_at_top_right,_#3b82f6,_transparent,_#db2777)]",
   },
   {
     title: "Now the best part this whole platform.",
@@ -40,7 +39,7 @@ const benefits = [
       "We developed a standard structure of representing the details on the resume. Having a standard structure unlocks a lot of benefits. Since the data is highly structured, AI can now make better use of it and perform activity like resume matching much more efficiently. Using standard format bring uniformity in the resume world where it becomes very easy to share and represent resume. Now, the resume pdf can be generated in different styles with same data with very less effort only because it is in standard format.",
     image: "/4.svg",
     background:
-      "bg-[radial-gradient(ellipse_at_bottom,_#0e7490,_transparent,_transparent)]",
+      "bg-[radial-gradient(circle_at_bottom,_#0e7490,_transparent,_transparent)]",
   },
 ];
 
@@ -76,9 +75,9 @@ function BenefitSection({
         />
       </div>
       <div className="w-full md:w-1/2">
-        <div className="p-10 pt-4 flex flex-col items-center">
+        <div className="p-10 max-sm:p-2 pt-4 flex flex-col items-center">
           <h2 className="text-xl font-semibold mb-2">{title}</h2>
-          <p className="text-sm">{description}</p>
+          <p className="text-sm text-justify">{description}</p>
         </div>
       </div>
     </section>
@@ -87,7 +86,7 @@ function BenefitSection({
 
 function SubscribeNow() {
   return (
-    <div className="w-2/3 flex flex-col md:flex-row items-center justify-center gap-4 mb-8">
+    <div className="flex flex-col md:flex-row items-center justify-center gap-4 mb-8">
       <Input placeholder="Email" type="email" />
       <Button>Subscribe Now</Button>
     </div>
@@ -96,7 +95,7 @@ function SubscribeNow() {
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-x-hidden">
       {/* Hero Section */}
       <section className="flex flex-col items-center justify-center w-full min-h-screen relative bg-[radial-gradient(circle_at_bottom,_#d97706,_transparent,_transparent)]">
         <div className="flex flex-col items-center text-center max-w-xl md:max-w-3xl">
@@ -113,7 +112,7 @@ export default function LandingPage() {
           <SubscribeNow />
         </div>
 
-        <div className="absolute bottom-0 max-lg:h-[18rem] lg:h-[23rem] pt-25 w-full flex flex-col items-center overflow-hidden">
+        <div className="absolute bottom-0 max-lg:h-[18rem] lg:h-[23rem] max-sm:h-[15rem] pt-25 w-full flex flex-col items-center overflow-hidden">
           <Image
             src="/resume.png"
             alt=""
@@ -126,7 +125,9 @@ export default function LandingPage() {
 
       {/* Benefits Section */}
       <section className="flex flex-col items-center justify-center w-full p-20">
-        <h1 className="text-4xl md:text-5xl">Why do i use Central#resume ?</h1>
+        <h1 className="text-4xl md:text-5xl max-sm:text-3xl">
+          Why do i use Central#resume?
+        </h1>
       </section>
       {benefits.map((benefit, index) => (
         <BenefitSection
@@ -141,7 +142,7 @@ export default function LandingPage() {
       ))}
 
       {/* Call to Action Section */}
-      <section className="flex flex-col items-center justify-center w-full p-52">
+      <section className="flex flex-col items-center justify-center w-full p-52 max-sm:p-0">
         <div className="w-3/4 flex flex-col items-center justify-center gap-4 mb-8 text-center">
           <h1 className="text-4xl md:text-5xl  pt-10 pb-5">
             Ready to transform your resume experience?
