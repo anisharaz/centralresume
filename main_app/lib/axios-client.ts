d741294f3660336c9fb7bfb6e4e5692d3f6a2c31
Anish Araz <anisharaz919@gmail.com>
2025-06-01 12:55:30 +0530

    FIX:[main_app] : build fix & landing page hero text

--- main_app/lib/axios-client.ts
+++ main_app/lib/axios-client.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const resumeBackendAxiosClient = axios.create({
-  baseURL: new URL(process.env.PROTOCOL_BE_URL as string).toString(),
+  baseURL: new URL(
+    process.env.PROTOCOL_BE_URL || "http://localhost:4000"
+  ).toString(),
   timeout: 1000,
 });
