2c7d67335255adaed1dac669292370ff56d57f77
Anish Araz <anisharaz919@gmail.com>
2025-06-11 11:54:20 +0530

    fix[main_app]: build fix and favicon

--- main_app/app/layout.tsx
+++ main_app/app/layout.tsx
@@ -1,15 +1,8 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Poppins } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["400", "500", "600", "700"],
-});
 
 const mono = Geist_Mono({
   variable: "--font-mono",
@@ -53,7 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${poppins.className} ${mono.variable} antialiased`}>
+      <body className={`${mono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
